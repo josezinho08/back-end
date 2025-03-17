7e094b5cf7c1ecd173dca407150a6351ad42adbe
josezinho08 <162449751+josezinho08@users.noreply.github.com>
2025-03-17 08:46:51 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,23 @@
+let custos = [800, 1000, 300, 500]
+let contador = 0
+let somatorio= 0
+while(contador < custos.length){
+    somatorio = somatorio + custos [contador];
+    contador = contador + 1;
+}
+console.log ("O seu custo mensal é " + somatorio);
+
+
+let ganhos = [680, 750, 300, 400]
+let contadorsalario = 0
+let somatoriosalario = 0
+while(contadorsalario < ganhos.length){
+    somatoriosalario = somatoriosalario + ganhos [contadorsalario];
+    contadorsalario = contadorsalario + 1;
+}
+console.log ("O seu ganhos mensal é " + somatoriosalario);
+let maximo = somatorio - somatorio
+console.log(typeof(somatoriosalario))
+console.log ("O eu saldo liquido é " + somatorio);
+
+
