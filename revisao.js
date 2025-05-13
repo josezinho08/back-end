a7171d3966b89540d018e3aa5172868bc28fe549
josezinho08 <162449751+josezinho08@users.noreply.github.com>
2025-05-13 12:08:48 -0300

    Add files via upload

---
+++ revisao.js
@@ -0,0 +1,38 @@
+// 1) Criação de variável de texto e numérica
+
+let nome = "José Gabriel de Souza"
+let chamada = 9 
+
+// 2) Criação de uma lista
+
+let jogos = ["free fire", "gta", "futebol"];
+    
+// 3) Utilização de um laço while
+
+console.log ("EXERCICIO 3")
+console.log ("Jogos favoritos de " + nome + " é:")
+ 
+let i = 0;
+while (i < 3){
+    console.log(jogos[i]);
+    i++;
+}
+
+// 4) Criação de um objeto
+
+//console.log ("Os filmes favoritos de " + nome + " é: ");
+
+let filme = {
+    flm: "Era do Gelo 3",
+    lançamento: 2009,
+    nota: 5.3
+};
+//console.log(filme.flm);
+//console.log(filme.lançamento);
+//console.log(filme.nota);
+
+// 5) Utilização de if-else
+
+if (filme.nota> 7.0) {
+    console.log ("O filme é bom");
+  } else if ()
